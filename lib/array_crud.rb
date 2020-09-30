deed317ae37e5538a4d33c294d755c70640e49d2
rational-architecture-1641 <jarmah241@gmail.com>
2020-09-30 16:27:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -31,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
